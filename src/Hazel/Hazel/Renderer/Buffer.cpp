dd995f9626f4c4dd71a5e780aa8820185500736a
wzy-0723 <3339312640@qq.com>
2026-06-28 18:04:19 +0800

    实现基础动态绘制

--- src/Hazel/Hazel/Renderer/Buffer.cpp
+++ src/Hazel/Hazel/Renderer/Buffer.cpp
@@ -5,7 +5,20 @@
 
 #include "OpenGLBuffer.h"
 
-namespace Hazel {
+namespace Hazel 
+{
+	Ref<VertexBuffer> VertexBuffer::Create(uint32_t size)
+	{
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::API::None:    HZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+			case RendererAPI::API::OpenGL:  return CreateRef<OpenGLVertexBuffer>(size);
+		}
+
+		HZ_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 
 	Ref<VertexBuffer> VertexBuffer::Create(float* vertices, uint32_t size)
 	{
