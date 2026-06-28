7c5bc428380c2a8a1394d403b2cdb6fe7c48eba5
wzy-0723 <3339312640@qq.com>
2026-06-28 19:10:33 +0800

    添加纹理到动态批渲染

--- src/Hazel/Platform/OpenGL/OpenGLShader.cpp
+++ src/Hazel/Platform/OpenGL/OpenGLShader.cpp
@@ -191,6 +191,11 @@ namespace Hazel
 		UploadUniformInt(name, value);
 	}
 
+	void OpenGLShader::SetIntArray(const std::string& name, int* values, uint32_t count)
+	{
+		UploadUniformIntArray(name, values, count);
+	}
+
 	void OpenGLShader::SetFloat(const std::string& name, float value)
 	{
 		HZ_PROFILE_FUNCTION();
@@ -221,6 +226,12 @@ namespace Hazel
 	{
 		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
 		glUniform1i(location, value);
+	}
+
+	void OpenGLShader::UploadUniformIntArray(const std::string& name, int* values, uint32_t count)
+	{
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform1iv(location, count, values);
 	}
 
 	void OpenGLShader::UploadUniformFloat(const std::string& name, float value)
