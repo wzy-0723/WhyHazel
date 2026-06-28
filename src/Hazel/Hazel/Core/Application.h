e6b40b653eea61f176830ae77a1e63240c33779e
wzy-0723 <3339312640@qq.com>
2026-06-28 19:20:59 +0800

    Commits on Apr 8, 2020

--- src/Hazel/Hazel/Core/Application.h
+++ src/Hazel/Hazel/Core/Application.h
@@ -41,9 +41,9 @@ namespace Hazel
 		void PushLayer(Layer* layer);
 		void PushOverlay(Layer* overlay);
 
-		inline Window& GetWindow() { return *m_Window; }
+		Window& GetWindow() { return *m_Window; }
 
-		inline static Application& Get() { return *s_Instance; }
+		static Application& Get() { return *s_Instance; }
 
 		
 
--- src/Hazel/Hazel/Core/Layer.h
+++ src/Hazel/Hazel/Core/Layer.h
@@ -20,7 +20,7 @@ namespace Hazel {
 		virtual void OnImGuiRender() {}
 		virtual void OnEvent(Event& event) {}
 
-		inline const std::string& GetName() const { return m_DebugName; }
+		const std::string& GetName() const { return m_DebugName; }
 	protected:
 		std::string m_DebugName;
 	};
