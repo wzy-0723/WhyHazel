e950726928ce5b4c81644d6bad9dca2b16203a17
wzy-0723 <3339312640@qq.com>
2026-07-01 18:20:04 +0800

    Commits on Oct 19, 2020

--- src/Hazel/Hazel/Scene/Scene.cpp
+++ src/Hazel/Hazel/Scene/Scene.cpp
@@ -47,7 +47,7 @@ namespace Hazel {
 
 		// Render 2D
 		Camera* mainCamera = nullptr;
-		glm::mat4* cameraTransform = nullptr;
+		glm::mat4 cameraTransform;
 		{
 			auto view = m_Registry.view<TransformComponent, CameraComponent>();
 			for (auto entity : view)
@@ -57,7 +57,7 @@ namespace Hazel {
 				if (camera.Primary)
 				{
 					mainCamera = &camera.Camera;
-					cameraTransform = &transform.Transform;
+					cameraTransform = transform.GetTransform();
 					break;
 				}
 			}
@@ -65,20 +65,18 @@ namespace Hazel {
 
 		if (mainCamera)
 		{
-			Renderer2D::BeginScene(*mainCamera, *cameraTransform);
+			Renderer2D::BeginScene(*mainCamera, cameraTransform);
 
 			const auto& group = m_Registry.group<TransformComponent>(entt::get<SpriteRendererComponent>);
 			for (auto entity : group)
 			{
 				auto [transform, sprite] = group.get<TransformComponent, SpriteRendererComponent>(entity);
 
-				Renderer2D::DrawQuad(transform, sprite.Color);
+				Renderer2D::DrawQuad(transform.GetTransform(), sprite.Color);
 			}
 
 			Renderer2D::EndScene();
 		}
-
-
 	}
 
 	void Scene::OnViewportResize(uint32_t width, uint32_t height)
