97d1b9979a0b3ebc78b73bfd0d8eaa4a3a17a68a
wzy-0723 <3339312640@qq.com>
2026-07-01 14:54:26 +0800

    Commits on Aug 26, 2020

--- src/Hazelnut/EditorLayer.h
+++ src/Hazelnut/EditorLayer.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "Hazel.h"
-
+#include "SceneHierarchyPanel.h"
 namespace Hazel
 {
 
@@ -40,5 +40,7 @@ namespace Hazel
 		glm::vec2 m_ViewportSize = { 0.0f, 0.0f };
 
 		glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
+		// Panels
+		SceneHierarchyPanel m_SceneHierarchyPanel;
 	};
 };
---
+++ src/Hazelnut/panels/SceneHierarchyPanel.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Base.h"
+#include "Log.h"
+#include "Scene.h"
+#include "Entity.h"
+
+namespace Hazel 
+{
+
+	class SceneHierarchyPanel
+	{
+	public:
+		SceneHierarchyPanel() = default;
+		SceneHierarchyPanel(const Ref<Scene>& scene);
+
+		void SetContext(const Ref<Scene>& scene);
+
+		void OnImGuiRender();
+	private:
+		void DrawEntityNode(Entity entity);
+	private:
+		Ref<Scene> m_Context;
+		Entity m_SelectionContext;
+	};
+
+}
