4a8b2f99d8aa55515a94cd61c0e889245d0ac13f
wzy-0723 <3339312640@qq.com>
2026-07-01 16:38:21 +0800

    befor assert

--- src/Hazel/Hazel/Scene/SceneCamera.h
+++ src/Hazel/Hazel/Scene/SceneCamera.h
@@ -12,8 +12,9 @@ namespace Hazel
 		SceneCamera();
 		virtual ~SceneCamera() = default;
 
-		void SetOrthographic(float size, float nearClip, float farClip);
 		void SetPerspective(float verticalFOV, float nearClip, float farClip);
+		void SetOrthographic(float size, float nearClip, float farClip);
+
 		void SetViewportSize(uint32_t width, uint32_t height);
 
 		float GetPerspectiveVerticalFOV() const { return m_PerspectiveFOV; }
